c98567fd3ecf3291bb2183f499e9c72eaa3007e5
sampathdissanayaka <dmcsampath97@gmail.com>
2020-10-29 00:12:03 +0700

    update app

--- app/src/main/java/com/example/calculaterapp/MainActivity.kt
+++ app/src/main/java/com/example/calculaterapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.calculaterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvOne.setOnClickListener { appendOnExpresstion("1",true)}
+        tvTwo.setOnClickListener { appendOnExpresstion("2",true)}
+        tvThree.setOnClickListener { appendOnExpresstion("3",true)}
+        tvFour.setOnClickListener { appendOnExpresstion("4",true)}
+        tvFive.setOnClickListener { appendOnExpresstion("5",true)}
+        tvSix.setOnClickListener { appendOnExpresstion("6",true)}
+        tvSeven.setOnClickListener { appendOnExpresstion("7",true)}
+        tvEight.setOnClickListener { appendOnExpresstion("8",true)}
+        tvNine.setOnClickListener { appendOnExpresstion("9",true)}
+
     }
+
+    fun appendOnExpresstion(string:String, canClear:Boolean){
+        if(canClear){
+            tvResult.text=""
+            tvExpression.append(string)
+        }else{
+            tvExpression.append(tvResult.text)
+            tvExpression.append(string)
+            tvResult.text=""
+        }
+    }
+
+
+
 }
